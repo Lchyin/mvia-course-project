20019c670b67b7da43a61c60e4be8c18f9362b7e
Monster <809287216@qq.com>
2020-02-28 12:30:37 +0800

    gcc&g++&make

---
+++ Hello_g++/Hello_world.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: Hello_world.cpp
+	> Author: amoscykl
+	> Mail: amoscykl@163.com 
+	> Created Time: Fri 28 Feb 2020 12:13:07 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(int argc,char *argv[])
+{
+	cout << "Hello_world"<<endl;
+	return 0;
+}
---
+++ Hello_gcc/Hello_world.c
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: Hello_world.c
+	> Author: amoscykl
+	> Mail: amoscykl@163.com 
+	> Created Time: Fri 28 Feb 2020 12:03:26 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+void main()
+{
+	printf("Hello_world\n");
+}
